6255f8ccc2d7200033df15170765e9f9073e69eb
Stephen Bolton <stephen@bitsonthemind.com>
2022-09-24 23:20:06 -0400

    Turn off logging

--- main.tf
+++ main.tf
@@ -12,12 +12,12 @@ provider "proxmox" {
   pm_tls_insecure = true
   pm_log_enable = true
   pm_parallel = 6
-  pm_log_file   = "terraform-plugin-proxmox.log"
-  pm_debug      = true
-  pm_log_levels = {
-    _default    = "debug"
-    _capturelog = ""
-  }
+  # pm_log_file   = "terraform-plugin-proxmox.log"
+  # pm_debug      = true
+  # pm_log_levels = {
+  #   _default    = "debug"
+  #   _capturelog = ""
+  # }
 }
 
 resource "proxmox_vm_qemu" "apoc-vms" {
