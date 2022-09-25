8bfbd2ba0ed62a3cfd55678ca20e4e6d4c6d0fa6
Stephen Bolton <stephen@bitsonthemind.com>
2022-09-25 00:30:28 -0400

    Create kubernetes nodes
    * Rename nodes to align with roles
    * Move ssh key to variable file

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
-resource "proxmox_vm_qemu" "apoc-vms" {
-  count = 2
-  name = "apoc-${count.index + 1}"
-  desc = "Testing out Terraform apply"
+resource "proxmox_vm_qemu" "kubernetes-controller" {
+  name = "kube-ctrl"
+  desc = "Kubernetes Controller Node"
   onboot = true
 
   target_node = "apoc"
@@ -34,18 +33,16 @@ resource "proxmox_vm_qemu" "apoc-vms" {
     ]
   }
 
-  ipconfig0 = "ip=192.168.1.2${count.index}/24,gw=192.168.1.254"
+  ipconfig0 = "ip=192.168.1.20/24,gw=192.168.1.254"
 
   ciuser = "serveradmin"
-  sshkeys = <<EOF
-  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOsUvi/j/2Gs8QkZ5S0/bGsK/BhmU8n24eDFCc7GZx9 cardno:14 404 815
-  EOF
+  sshkeys = var.ssh_key
 }
 
-resource "proxmox_vm_qemu" "mouse-vms" {
+resource "proxmox_vm_qemu" "Kubernetes-workers" {
   count = 2
-  name = "mouse-${count.index + 1}"
-  desc = "Testing out Terraform apply"
+  name = "kube-worker-${count.index + 1}"
+  desc = "Kubernetes Worke Nodes"
   onboot = true
 
   target_node = "mouse"
@@ -81,9 +78,7 @@ resource "proxmox_vm_qemu" "mouse-vms" {
   ipconfig0 = "ip=192.168.1.3${count.index}/24,gw=192.168.1.254"
 
   ciuser = "serveradmin"
-  sshkeys = <<EOF
-  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOsUvi/j/2Gs8QkZ5S0/bGsK/BhmU8n24eDFCc7GZx9 cardno:14 404 815
-  EOF
+  sshkeys = var.ssh_key
 }
 
 resource "proxmox_vm_qemu" "tank-vms" {
@@ -125,7 +120,5 @@ resource "proxmox_vm_qemu" "tank-vms" {
   ipconfig0 = "ip=192.168.1.4${count.index}/24,gw=192.168.1.254"
 
   ciuser = "serveradmin"
-  sshkeys = <<EOF
-  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOsUvi/j/2Gs8QkZ5S0/bGsK/BhmU8n24eDFCc7GZx9 cardno:14 404 815
-  EOF
+  sshkeys = var.ssh_key
 }
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "ssh_key" {
+  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIOsUvi/j/2Gs8QkZ5S0/bGsK/BhmU8n24eDFCc7GZx9 cardno 14 404 815"
+}
